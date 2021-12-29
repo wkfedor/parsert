a1c10d7b33ff08c5b9849a2549ffbabc0159ab3a
wkfedor <wkfedor@gmail.com>
2021-12-29 07:39:22 +0700

    2 post

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,15 +3,24 @@ class MessagesController < ApplicationController
         @ms=Message.all
     end
 
+
+    def show
+        @ms=Message.find(params[:id])
+    end
+
     def new
 
     end
 
     def create
-        render plain: params[:addmessage].inspect
+        #render plain: params[:addmessage].inspect
+        @temp=Message.new(message_params)
+        @temp.save
+        redirect_to @temp
     end
 
     private def message_params
+    params.require(:addmessage).permit(:url, :head, :pr, :text,  :link, :linksx, :dictionary, :photo, :phone)
     end
 
 end
